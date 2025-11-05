678fe4a92a7d7941aee6bba40e1253dc7685a8e1
YaroslavP <slawa.pechyonkin7@yandex.ru>
2025-11-05 13:34:49 +0300

    fix main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     tennis_match_scoreboard.start_server()
     
     tennis_match_scoreboard: TennisMatchScoreboard = TennisMatchScoreboard(settings.SERVER_HOST, settings.SERVER_PORT) # .simple_app
-    application = WhiteNoise(tennis_match_scoreboard, root=r"C:\Users\slawa\Desktop\Python\00.Pet_Projects\07.TennisMatchScoreboard\TennisMatchScoreboard")
+    # application = WhiteNoise(tennis_match_scoreboard, root=r"C:\Users\slawa\Desktop\Python\00.Pet_Projects\07.TennisMatchScoreboard\TennisMatchScoreboard")
+    application = WhiteNoise(tennis_match_scoreboard, root=r"..")
     # application.add_files("/path/to/more/static/files", prefix="more-files/")
     # tennis_match_scoreboard.start_server()
     application.application.start_server()
